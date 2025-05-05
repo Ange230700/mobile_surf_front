f978e189c7e199ebbbd5d5bc2763d1cadc8e3fdc
Ange230700 <kouakouangeericstephane@gmail.com>
2025-05-06 00:33:33 +0200

    do stuff

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'page/home.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(const MainApp());
