45b5582892929ac1e5318ed1845b136893bd40a8
Ange230700 <kouakouangeericstephane@gmail.com>
2025-05-07 12:32:08 +0200

    feat: 🎸  implement interactive maps part 3 (WIP)

--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 import '../utils/position.dart';
 
@@ -61,8 +62,9 @@ class DetailPage extends StatelessWidget {
                 children: [
                   TileLayer(
                     urlTemplate:
-                        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    subdomains: const ['a', 'b', 'c'],
+                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    tileProvider: CancellableNetworkTileProvider(),
+                    maxZoom: 19,
                   ),
                   MarkerLayer(
                     markers: [
